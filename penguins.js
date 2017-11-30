94cb6cd725bcf34f1345b557ceb285a73a7de8b2
elisesaxon <elisesaxon@gmail.com>
2017-11-29 22:40:06 -0800

    Change back to using Javascript, attempt to add elements to DOM

--- penguins.js
+++ penguins.js
@@ -1,7 +1,31 @@
 //https://googlecreativelab.github.io/coder-projects/projects/pop_up_penguins/
 
-$(document).ready(function() {
-	$("body").css("background-color", "#ccf5f5");
+window.onload = function() {
+	document.body.style.backgroundColor = "#ccf5f5";
 	
+    /*var para = document.createElement("P");
+    var t = document.createTextNode("This is a paragraph.");
+    para.appendChild(t);
+    document.body.appendChild(para);*/
+	var dropdown = document.createElement("select");
+	for(i=0; i<=64; i++) {
+		var option = dropdown.appendChild(option);
+		option.value = i;
+		option.attribute = i;
+		
+		dropdown.appendChild(option);
+	}
+		
+	document.appendChild(dropdown);
 	
-});
+	/*dropdown.onchange = selectNumPenguins(value);
+	
+	<select id="animationmenu" onchange="changeAnimation(this.value)">
+			<option selected value="Blank">Blank</option>
+			<option value="Exercise">Exercise</option>
+			<option value="Juggler">Juggler</option>
+			<option value="Bike">Bike</option>
+			<option value="Dive">Dive</option>
+			<option value="Custom">Custom</option>
+		</select>*/
+}
