5c58aa916320c41b31d32399d47c30fde1459390
elisesaxon <elisesaxon@gmail.com>
2017-11-29 23:35:36 -0800

    Add onchange event to dropdown menu

--- penguins.js
+++ penguins.js
@@ -1,27 +1,29 @@
 //https://googlecreativelab.github.io/coder-projects/projects/pop_up_penguins/
 
+
 window.onload = function() {
 	document.body.style.backgroundColor = "#ccf5f5";
+	
+	
+	/*var gameholder = document.createElement("div");
+	gameholder.id = "gameholder";
+	
+	var titlediv = document.createElement("div");
+	*/
+	
+	
+	var numPenguins;
     
 	var dropdown = document.createElement("select");
 	for(i=0; i<=64; i++) {
 		var option = document.createElement("option");
 		option.value = i;
 		option.text = String(i);
-		
 		dropdown.options.add(option);
 	}
+	dropdown.onchange = function() { numPenguins = this.value };
 		
 	document.body.appendChild(dropdown);
+
 	
-	/*dropdown.onchange = selectNumPenguins(value);
-	
-	<select id="animationmenu" onchange="changeAnimation(this.value)">
-			<option selected value="Blank">Blank</option>
-			<option value="Exercise">Exercise</option>
-			<option value="Juggler">Juggler</option>
-			<option value="Bike">Bike</option>
-			<option value="Dive">Dive</option>
-			<option value="Custom">Custom</option>
-		</select>*/
 }
