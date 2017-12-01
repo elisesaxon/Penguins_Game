d86fa2316699d8d0934a64033c0baf5ddcc2fb5c
elisesaxon <elisesaxon@gmail.com>
2017-11-30 18:00:15 -0800

    Render mounds for each penguin

--- penguins.js
+++ penguins.js
@@ -4,7 +4,6 @@
 window.onload = function() {
 	document.body.style.backgroundColor = "#ccf5f5";
 	
-	
 	var gameholder = document.createElement("div");
 	gameholder.id = "gameholder";
 	document.body.appendChild(gameholder);
@@ -16,34 +15,15 @@ window.onload = function() {
 	
 	var titlediv = document.createElement("div");
 	titlediv.id = "title";
-	document.body.appendChild(titlediv);
+	gameholder.appendChild(titlediv);
 	var apdtitlediv = document.getElementById("title");
 	apdtitlediv.style.width = "600px";
-	apdtitlediv.style.marginLeft = "auto";
-	apdtitlediv.style.marginRight = "auto";
 	apdtitlediv.style.height = "150px";
 	apdtitlediv.style.backgroundImage = "url('penguin_pngs/penguin_title.png')";
-	
-	var peng1 = document.createElement("div");
-	var peng2 = document.createElement("div");
-	var peng3 = document.createElement("div");
-	var peng4 = document.createElement("div");
-	var peng5 = document.createElement("div");
-	var peng6 = document.createElement("div");
-	var peng7 = document.createElement("div");
-	var peng8 = document.createElement("div");
-	peng1.class = "penguin1";
-	peng2.class = "penguin2";
-	peng3.class = "penguin3";
-	peng4.class = "penguin4";
-	peng5.class = "penguin5";
-	peng6.class = "penguin6";
-	peng7.class = "penguin7";
-	peng8.class = "penguin8";
-	
+		
 	
 	var numPenguins;
-    
+	
 	var dropdown = document.createElement("select");
 	for(i=0; i<=64; i++) {
 		var option = document.createElement("option");
@@ -52,8 +32,28 @@ window.onload = function() {
 		dropdown.options.add(option);
 	}
 	dropdown.onchange = function() { numPenguins = this.value };
+	
+	//document.body.appendChild(dropdown);
+	gameholder.appendChild(dropdown);
+	
+	
+	for(i=1; i<=8; i++) {
+		var pengdiv = document.createElement("div");
+		var namestr = "penguin" + String(i);
+		pengdiv.className = namestr;
+		//document.body.appendChild(peng);
+		gameholder.appendChild(pengdiv);
 		
-	document.body.appendChild(dropdown);
-
+		var moundname = "mound_" + String(i);
+		var peng = document.getElementsByClassName(namestr);
+		peng[0].style.width = "200px";
+		peng[0].style.height = "200px";
+		peng[0].style.cssFloat = "left";
+		peng[0].style.backgroundImage = "url('penguin_pngs/" + moundname + ".png')";
+	}
+	
+	var yetidiv = document.createElement("div");
+	yetidiv.className = "yeti";
+	gameholder.appendChild(yetidiv);
 	
 }
