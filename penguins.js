1fc6070b7a57b6f09f2fe23b30c54d0eeba1d9fe
elisesaxon <elisesaxon@gmail.com>
2017-12-01 14:42:21 -0800

    Trying to change the location of yeti each time, but it doesn't update onChange of dropdown

--- penguins.js
+++ penguins.js
@@ -20,15 +20,8 @@ window.onload = function() {
 	apdtitlediv.style.width = "600px";
 	apdtitlediv.style.height = "150px";
 	apdtitlediv.style.backgroundImage = "url('penguin_pngs/penguin_title.png')";
-		
-	
-	var numPenguins;
 	
 	var dropdown = document.createElement("select");
-	/*var option = document.createElement("option");
-	option.value = 0;
-	option.text = "num penguins";
-	dropdown.options.add(option);*/
 	for(i=2; i<=64; i++) {
 		var option = document.createElement("option");
 		option.value = i;
@@ -37,76 +30,88 @@ window.onload = function() {
 	}
 	
 	dropdown.onchange = function() { 
-		numPenguins = this.value; 
-	
-		//var allpenguins = [];
+		var numPenguins = this.value; 
+		
+		var yetiplace = Math.floor(Math.random() * numPenguins) + 1;
+		
 		var num = numPenguins;
+		while (num > 0) {
+			
+			if(num == yetiplace) {
+				var yetidiv = document.createElement("div");
+				yetidiv.className = "yeti";
+				gameholder.appendChild(yetidiv);
 	
-		while (num > 1) {
-			var i = (num % 8) + 1;
+				var yeti = document.getElementsByClassName("yeti");
+				yeti[0].style.width = "200px";
+				yeti[0].style.height = "200px";
+				yeti[0].style.cssFloat = "left";
+				yeti[0].style.backgroundImage = "url('penguin_pngs/mound_9.png')";
+				
+			} else {
+				var i = num;
+				var a = (num%8)+1;
 			
-			console.log("NUM: "+numPenguins + "   i: "+i);
-			
-			var pengdiv = document.createElement("div");
-			var namestr = "penguin" + String(i);
-			pengdiv.className = namestr;
-			gameholder.appendChild(pengdiv);
-		
-			var moundname = "mound_" + String(i);
-			var peng = document.getElementsByClassName(namestr);
-			peng[0].style.width = "200px";
-			peng[0].style.height = "200px";
-			peng[0].style.cssFloat = "left";
-			peng[0].style.backgroundImage = "url('penguin_pngs/" + moundname + ".png')";
-			
-			var namestr = "penguin" + String(i);
-			var peng = document.getElementsByClassName(namestr);
-		
-			peng[0].onmouseenter = function() {
-				this.style.backgroundImage = "url('penguin_pngs/mound_" + String(i) + "_hover.png')";
-				this.style.cursor = "pointer";
-			}
-		
-			peng[0].onmouseleave = function() {
-				this.style.backgroundImage = "url('penguin_pngs/mound_" + String(i) + ".png')";
-				this.style.cursor = "auto";
-			}
-		
-			peng[0].onmousedown = function() {
-				this.style.backgroundImage = "url('penguin_pngs/penguin_" + String(i) + ".png')";
-			}
+				var pengdiv = document.createElement("div");
+				var namestr = "penguin" + String(i);
+				pengdiv.className = namestr;
+				gameholder.appendChild(pengdiv);
+				
+				var moundname = "mound_" + String(a);
+				var peng = document.getElementsByClassName(namestr);
+				peng[0].style.width = "200px";
+				peng[0].style.height = "200px";
+				peng[0].style.cssFloat = "left";
+				peng[0].style.backgroundImage = "url('penguin_pngs/" + moundname + ".png')";
+			}	
 			
 			num -= 1;
-			
-			
-			
-			var yetidiv = document.createElement("div");
-			yetidiv.className = "yeti";
-			gameholder.appendChild(yetidiv);
-	
-			var yeti = document.getElementsByClassName("yeti");
-			yeti[0].style.width = "200px";
-			yeti[0].style.height = "200px";
-			yeti[0].style.cssFloat = "left";
-			yeti[0].style.backgroundImage = "url('penguin_pngs/mound_9.png')";
-	
-			yeti[0].onmouseenter = function() {
-				this.style.backgroundImage = "url('penguin_pngs/mound_9_hover.png')";
-				this.style.cursor = "pointer";
-			}
-			
-			yeti[0].onmouseleave = function() {
-				this.style.backgroundImage = "url('penguin_pngs/mound_9.png')";
-				this.style.cursor = "auto";
-			}
-	
-			yeti[0].onmousedown = function() {
-				this.style.backgroundImage = "url('penguin_pngs/yeti.png')";
-				var timer = setTimeout(function () {
-					window.alert("Yaaaarrrrr!");
-				}, 250);
-			}
 		}
+		
+		let num2 = numPenguins;
+		while (num2 > 0) {
+			if(num2 == yetiplace) {
+				var yeti = document.getElementsByClassName("yeti");
+				
+				yeti[0].onmouseenter = function() {
+					this.style.backgroundImage = "url('penguin_pngs/mound_9_hover.png')";
+					this.style.cursor = "pointer";
+				}
+				
+				yeti[0].onmouseleave = function() {
+					this.style.backgroundImage = "url('penguin_pngs/mound_9.png')";
+					this.style.cursor = "auto";
+				}
+	
+				yeti[0].onmousedown = function() {
+					this.style.backgroundImage = "url('penguin_pngs/yeti.png')";
+					var timer = setTimeout(function () {
+						window.alert("Yaaaarrrrr!");
+					}, 250);
+				}
+			
+			} else {
+				let a = (num2%8)+1;
+				var namestr = "penguin" + String(num2);
+				var peng = document.getElementsByClassName(namestr);
+		
+				peng[0].onmouseenter = function() {
+					this.style.backgroundImage = "url('penguin_pngs/mound_" + String(a) + "_hover.png')";
+					this.style.cursor = "pointer";
+				}
+			
+				peng[0].onmouseleave = function() {
+					this.style.backgroundImage = "url('penguin_pngs/mound_" + String(a) + ".png')";
+					this.style.cursor = "auto";
+				}
+			
+				peng[0].onmousedown = function() {
+					this.style.backgroundImage = "url('penguin_pngs/penguin_" + String(a) + ".png')";
+				}
+			}
+			
+			num2 -= 1;
+		}	
 	}
 	gameholder.appendChild(dropdown);
 }
