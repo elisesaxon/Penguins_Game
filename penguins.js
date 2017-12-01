ab428d36971fcaa215abf223726da5d2d7d05334
elisesaxon <elisesaxon@gmail.com>
2017-11-30 18:25:53 -0800

    Change mound images on hover

--- penguins.js
+++ penguins.js
@@ -1,7 +1,7 @@
 //https://googlecreativelab.github.io/coder-projects/projects/pop_up_penguins/
 
-
 window.onload = function() {
+	
 	document.body.style.backgroundColor = "#ccf5f5";
 	
 	var gameholder = document.createElement("div");
@@ -31,9 +31,8 @@ window.onload = function() {
 		option.text = String(i);
 		dropdown.options.add(option);
 	}
-	dropdown.onchange = function() { numPenguins = this.value };
+	dropdown.onchange = function() { numPenguins = this.value; }
 	
-	//document.body.appendChild(dropdown);
 	gameholder.appendChild(dropdown);
 	
 	
@@ -41,7 +40,6 @@ window.onload = function() {
 		var pengdiv = document.createElement("div");
 		var namestr = "penguin" + String(i);
 		pengdiv.className = namestr;
-		//document.body.appendChild(peng);
 		gameholder.appendChild(pengdiv);
 		
 		var moundname = "mound_" + String(i);
@@ -50,10 +48,39 @@ window.onload = function() {
 		peng[0].style.height = "200px";
 		peng[0].style.cssFloat = "left";
 		peng[0].style.backgroundImage = "url('penguin_pngs/" + moundname + ".png')";
+		
+		var moundhover = moundname + "_hover";
+		peng[0].onmouseenter = function() {
+			this.style.backgroundImage = "url('penguin_pngs/" + moundhover + ".png')";
+			this.style.cursor = "pointer";
+		}
+		
+		peng[0].onmouseleave = function() {
+			this.style.backgroundImage = "url('penguin_pngs/" + moundname + ".png')";
+			this.style.cursor = "auto";
+		}
 	}
 	
 	var yetidiv = document.createElement("div");
 	yetidiv.className = "yeti";
 	gameholder.appendChild(yetidiv);
+	
+	var yeti = document.getElementsByClassName("yeti");
+	yeti[0].style.width = "200px";
+	yeti[0].style.height = "200px";
+	yeti[0].style.cssFloat = "left";
+	yeti[0].style.backgroundImage = "url('penguin_pngs/mound_9.png')";
+	
+	yeti[0].onmouseenter = function() {
+		this.style.backgroundImage = "url('penguin_pngs/mound_9_hover.png')";
+		this.style.cursor = "pointer";
+	}
+		
+	yeti[0].onmouseleave = function() {
+		this.style.backgroundImage = "url('penguin_pngs/mound_9.png')";
+		this.style.cursor = "auto";
+	}
+	
+	
 	
 }
